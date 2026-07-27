03cd60b2765b4019fe8f96e112035c7e0ec85131
Yevhennn <eugenefun@hotmail.com>
2026-07-28 01:05:23 +0300

    add chat_bot

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ChatbotController;
 
 Route::get('/lang/{locale}', [LanguageController::class, 'switchLang'])->name('lang.switch');
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-
 Route::post('/installation-order', [OrderController::class, 'storeInstallationOrder'])->name('installation.order');
 
+// Chatbot route
+Route::post('/chatbot/ask', [ChatbotController::class, 'ask'])->name('chatbot.ask');
